9a58640ad5dc17fd364068d45eaf888a77cf31ad
Doaa Mosalam <DoaaMosalam9@gmail.com>
2024-06-19 23:20:34 +0300

    Infinity store app

---
+++ app/src/main/java/com/doaamosallam/infinitystore/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.doaamosallam.infinitystore
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+}
---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+
+buildscript {
+    repositories {
+        mavenLocal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.androidLibrary) apply false
+}
