10db5dc2af7418e405a0f37b659e1f93663caa53
Doaa Mosallam <Doaamosalam9@gmail.com>
2024-07-02 04:12:23 +0300

    create login screen by compose

--- app/src/main/java/com/doaamosallam/infinitystore/MainActivity.kt
+++ app/src/main/java/com/doaamosallam/infinitystore/MainActivity.kt
@@ -31,8 +31,6 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             LoginScreen()
-
-//            InfinityStoreApp()
         }
     }
 
---
+++ app/src/main/java/com/doaamosallam/infinitystore/modifier/Button.kt
@@ -0,0 +1,44 @@
+package com.doaamosallam.infinitystore.modifier
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AuthButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    buttonText: String,
+    buttonColor: Color,
+    textColor: Color
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.width(250.dp).height(40.dp),
+        shape = RoundedCornerShape(20.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = buttonColor,
+            contentColor = textColor
+        ),
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 10.dp,
+            pressedElevation = 10.dp
+        )
+    ) {
+        Text(
+            text = buttonText,
+            style = MaterialTheme.typography.labelSmall // Optional: Adjust text style as needed
+        )
+    }
+    }
